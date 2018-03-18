3600eecbb0696300cba535f0d0fadfb40d30630a
Daria <dashasemenyuta@gmail.com>
2018-03-18 20:03:01 +0200

    add mergeSearch() method

--- src/project/Main.java
+++ src/project/Main.java
@@ -4,23 +4,37 @@ public class Main {
 
     public static void main(String[] args) {
         int[] arr = {1, 3, -9, 5, 12, 9};
+        //check min, max of array
         int min = findMin(arr);
         int max = findMax(arr);
         System.out.println("Min of array is "+ min );
         System.out.println("Max of array is "+ max );
-        
+        //check bubbleSort
         bubbleSort(arr);
-        
+        //check quickSort
         quickSort(arr, 0, arr.length );
-
-        for(int i=0; i < arr.length ; ++i){
-            System.out.println(arr[i]);
-        }
-
+        print(arr);
+        //check binarySearch
         int[] arr2 = {0, 1, 3, 6, 7, 9};
-
-        int indx = binarySearch(arr2, 7, 0, arr2.length-1);
+        System.out.println("Second array:");
+        print(arr2);
+        int indx = binarySearch(arr2, 9, 0, arr2.length-1);
         System.out.println("Index = "+indx);
+        //check mergeSort
+        int[] arr3 = {8, 2, 4, 1};
+        System.out.println("Array before sort:");
+        print(arr3);
+        mergeSort(arr3, 0, arr3.length-1);
+        System.out.println("\nArray after sort:");
+        print(arr3);
+
+    }
+
+    public   static  void print(int[] arr){
+        System.out.print("\n");
+        for(int i=0; i < arr.length ; ++i){
+            System.out.print(arr[i]+" ");
+        }
     }
 
     private static int findMin(int[] arr){
@@ -54,9 +68,7 @@ public class Main {
                 }
             }
         }
-        for(int i = 0; i < arr.length; ++i ){
-            System.out.print(arr[i]+" ");
-        }
+        print(arr);
     }
 
     public static void quickSort(int arr[], int start, int stop ) {
@@ -77,11 +89,7 @@ public class Main {
         quickSort(arr, start, ind);
         quickSort(arr, ind+1, arr.length);
     }
-
-    public static void mergeSort(){
-
-    }
-
+    //binarySearch() method with recursion
     public static int binarySearch(int[] arr, int element, int left, int right){
 
         int indx = (right + left)/2;
@@ -89,7 +97,7 @@ public class Main {
 
             if (element < arr[indx])
                 indx = binarySearch(arr, element, left, indx);
-            if (element > arr[indx])
+            else
                 indx = binarySearch(arr, element, indx + 1, right);
         }
 
@@ -97,6 +105,40 @@ public class Main {
 
     }
 
+    public static void mergeSort(int[] arr, int left, int right){
+        if ((right-left) < 1 )
+            return;
+            int countL = (right + left) / 2 + 1;
+            int countR = right - (right + left) / 2;
+            int[] arrL = new int[countL];
+            int[] arrR = new int[countR];
 
+            for (int i = 0; i < countL; ++i)
+                arrL[i] = arr[i + left];
+            for (int i = 0; i < countR; ++i)
+                arrR[i] = arr[countL + i];
 
+            mergeSort(arrL, left, countL - 1);
+
+            mergeSort(arrR, countL, countR - 1);
+            print(arrL);
+            print(arrR);
+            int r = 0;
+            int l = 0;
+            int mCount = 0;
+
+            while ((l < countL) && (r < countR)) {
+                if (arrL[l] < arrR[r]) {
+                    arr[mCount] = arrL[l];
+                    arr[mCount+1] = arrR[r];
+                    ++l;
+                } else {
+                    arr[mCount] = arrR[r];
+                    arr[mCount+1] = arrL[l];
+                    ++r;
+                }
+                mCount=+2;
+
+        }
+    }
 }
